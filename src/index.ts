1fafb763c796a7fda8b794038d3ce8b4157184c0
Glenn Rempe <glenn@truestamp.com>
2021-09-29 10:36:02 -0400

    Add POST /entries and GET /entries endpoints, using KV as the backing store

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
+// Copyright © 2020-2021 Truestamp Inc. All rights reserved.
+
 import { Router } from 'worktop'
 import * as Cache from 'worktop/cache'
 
+import { ulid, toHEX } from 'worktop/utils'
+
+import { read, write, paginate } from 'worktop/kv'
+import type { KV } from 'worktop/kv'
+
+declare const OBSERVABLE_ENTROPY: KV.Namespace
+
 const SHA1_REGEX = /^(?:(0x)*([A-Fa-f0-9]{2}){20})$/i
 const SHA256_REGEX = /^(?:(0x)*([A-Fa-f0-9]{2}){32})$/i
+
+// Hex string 32 bytes (2 hex chars each byte group)
+const HEX_REGEX = /^(([A-Fa-f0-9]{2}){32})$/i
+
+interface Entry {
+  entropy: string
+  for?: string
+}
+
+function getNowInUnixSeconds(): number {
+  const nowInSeconds = Math.floor(Date.now() / 1000)
+  return parseInt(nowInSeconds.toString(), 10)
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
+}
+
+async function digestMessage(message: string) {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(message)
+  const hash = await crypto.subtle.digest('SHA-256', data)
+  return hash
+}
 
 async function fetchJsonFromURL(url: string) {
   // w/ cloudflare caching
@@ -48,7 +84,6 @@ const fetchEntropy = async (id: string | null = null) => {
   return entropy
 }
 
-// Initialize
 const API = new Router()
 
 API.add('GET', '/', async (req, res): Promise<void> => {
@@ -116,6 +151,114 @@ API.add('GET', '/hash/:hash', async (req, res) => {
       res.send(404, `Not Found : ${error.message}`)
     }
   }
+})
+
+API.add('POST', '/entries', async (req, res) => {
+  let entry: Entry
+  try {
+    const bodyEntry = await req.body<Entry>()
+    if (!bodyEntry || !bodyEntry.entropy.trim()) {
+      return res.send(422, {
+        error: true,
+        message: 'Bad Request : Missing JSON body object with entropy key.',
+      })
+    }
+    entry = bodyEntry
+  } catch (error) {
+    return res.send(400, {
+      error: true,
+      message: 'Bad Request : Missing JSON body object with entropy key.',
+    })
+  }
+
+  if (!HEX_REGEX.test(entry.entropy)) {
+    return res.send(400, {
+      error: true,
+      message:
+        'Bad Request : Invalid entropy value, must be 32 byte hex string (64 chars)',
+    })
+  }
+
+  const key = `entry::${ulid()}`
+
+  // Auto-expire all entries after a random interval between 10 and 60 minutes from now
+  // To ensure that the entry is unpredictably present in at least one, and possibly
+  // up to an hour's worth of observable entropy runs.
+  const expiration: number = getNowInUnixSeconds() + 60 * getRandomInt(10, 60)
+
+  const hashedEntry = await digestMessage(
+    `${key}::${expiration}::${entry.entropy}`,
+  )
+
+  // For each entry, store the hashed entry as well. This prevents the perception that someone could
+  // use a chosen entry value to 'poison' the observable entropy pool as there will always be an unpredictable
+  // additional entry added alongside the one they submitted.
+  const successWriteHashedEntry = await write<Entry>(
+    OBSERVABLE_ENTROPY,
+    `${key}::hashed`,
+    { entropy: toHEX(hashedEntry), for: key },
+    {
+      expiration,
+    },
+  )
+
+  if (!successWriteHashedEntry) {
+    return res.send(500, {
+      error: true,
+      message: 'Internal Server Error : Failed to write hashed entry.',
+    })
+  }
+
+  const successWriteEntry = await write<Entry>(OBSERVABLE_ENTROPY, key, entry, {
+    expiration,
+  })
+
+  if (successWriteEntry) {
+    res.send(201, {
+      key,
+      entropy: entry.entropy,
+      expiration,
+    })
+  } else {
+    res.send(500, {
+      error: true,
+      message: 'Internal Server Error : Failed to write entry.',
+    })
+  }
+})
+
+API.add('GET', '/entries', async (req, res) => {
+  const prefix = 'entry::'
+  const keys = await paginate<string[]>(OBSERVABLE_ENTROPY, { prefix })
+
+  const entryKeys = keys.map((key) => {
+    return key
+  })
+
+  const kvEntries: string[] = []
+  for (const e of entryKeys) {
+    // retrieve the entry from KV store
+    const kvEntry = await read(OBSERVABLE_ENTROPY, e, {
+      type: 'text',
+      metadata: true,
+    })
+
+    if (!kvEntry || !kvEntry.value) {
+      console.log(`kvEntry is null : ${kvEntry}`)
+      continue
+    }
+
+    const parsedKvEntry = JSON.parse(kvEntry.value)
+    if (!parsedKvEntry || !parsedKvEntry.entropy) {
+      console.log(`parsedKvEntry is null : ${parsedKvEntry}`)
+      continue
+    }
+
+    kvEntries.push(parsedKvEntry.entropy)
+  }
+
+  res.setHeader('Cache-Control', 'public, max-age=10')
+  res.send(200, kvEntries)
 })
 
 // Attach "fetch" event handler
